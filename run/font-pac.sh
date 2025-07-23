759464d8cd4a52f5ef58f2548137e28d01941189
capnbiggin <capnbiggin@proton.me>
2025-07-22 22:28:11 -0500

    automatid commit

--- run/font-pac.sh
+++ run/font-pac.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 NC='\033[0m'
@@ -7,16 +6,15 @@ RED='\033[0;31m'
 
 # Fonts
 FONTS=(
- ttf-jetbrains-mono-nerd
- ttf-font-awesome
- ttf-roboto
- noto-fonts
- noto-fonts-emoji
+  ttf-jetbrains-mono-nerd
+  ttf-font-awesome
+  ttf-roboto
+  noto-fonts
+  noto-fonts-emoji
 )
-
 
 # Source Utility function
 source ./utils.sh
 
 printf "\n$GREEN}Installing Fonts...${NC}\n"
-install_packages "${FONTS[@]"
+install_packages "${FONTS[@]}"
