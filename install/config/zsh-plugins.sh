a0c039cf1c3a32d8cc47a57e0b419f8379141839
capn <capnbiggin@proton.me>
2025-08-03 14:42:14 -0500

    automatic commit

--- install/config/zsh-plugins.sh
+++ install/config/zsh-plugins.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
 C='\033[0m'
-G='\033[0;32m'
-R='\033[0;31m'
 B='\033[0;34m'
-Y='\033[0;33m'
-
-printf "\n${B}making directory for the Z Shell config files and plugins${C}\n"
-mkdir -p ~/.config/zsh/plugins/
-sleep 0.2
 
 printf "\n${B}Cloning plugin zsh-syntax-highlighting${C}\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting/
@@ -16,7 +9,7 @@ sleep 0.2
 printf "\n${B}Cloning plugin zsh-autosuggestions${C}\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/plugins/zsh-autosuggestions/
 sleep 0.2
-printf "\b${B}Cloning plugin zsh-you-should-use${C}\n"
+printf "\n${B}Cloning plugin zsh-you-should-use${C}\n"
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ~/.config/zsh/plugins/zsh-you-should-use/
 sleep 0.2
 printf "\n${B}Cloning plugin zsh-history-substring-search${C}\n"
--- install/config/zsh.sh
+++ install/config/zsh.sh
@@ -12,25 +12,29 @@ set -e
 
 pushd ~
 
-printf "\n${Y}Checking if Z Shell is inatalled.${C}\n"
-if ! command -v zsh &>/dev/null; then
-  printf "\n${B}installing Z Shell, which and Stow GNU${C}/n"
-  sudo pacman -S --needed --noconfirm zsh which stow
-  sleep 0.2
-else
-  printf "${R}Removing old Z Shell configs.${C}\n"
-  rm -rf $ZDOTDIR/.zshrc $ZDOTDIR/.zprofile ~/.zshrc ~/.zprofile ~/.zshenv
-fi
+printf "\n${B}installing Z Shell, which and Stow GNU${C}/n"
+sudo pacman -S --needed --noconfirm zsh which
+sleep 0.2
+printf "\n${R}Removing old Z Shell configs.${C}\n"
+[ -f ~/.zshrc ] && rm -rf ~/.zshrc ~/.zprofile
+[ -f ~/.zshenv ] && rm -rf ~/.zshenv
+[ -f $ZDOTDIR/.zshrc ] && rm -rf $ZDOTDIR/.zshrc $ZDOTDIR/.zprofile
 
-. zsh-plugins.sh
+printf "\n${B}Downloading Z Shell plugins${C}\n"
+mkdir -p ~/.config/zsh/plugins
+source ~/dotfiles/config/Install/config/zsh-plugins.sh
 
+printf "\n${G}Changing default shell to zsh${C}\n"
 hash -r
 chsh -s $(which zsh)
+
+printf "\n${B}linking zsh configs${C}\n"
+ln -s ~/dotfiles/config/.zshenv ~/.zshenv
+ln -s ~/dotfiles/config/.config/zsh/.zprofile ~/.config/zsh/.zprofile
+ln -s ~/dotfiles/config/.config/zsh/.zshrc ~/.config/zsh/.zshrc
 
 sleep 0.2
 
 popd
 
-printf "\n${G}You will now need to restart your PC to use Z Shell.${C}\n"
-
-exit
+printf "\n${G}You will now need to 'reboot' to use Z Shell.${C}\n"
