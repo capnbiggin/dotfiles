5f7db699b7ee41016d2f900c44011dfb486d20a9
capn <capnbiggin@proton.me>
2025-08-03 00:14:46 -0500

    automatic commit

---
+++ install/desktops/hyprland.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+C='\033[0m'    # no color
+G='\033[0;32m' # green color
+R='\033[0;31m' # red color
+
+HYPR=(
+  hyprland-git
+  hypridle
+  hyprpicker
+  hyprlock
+  swww
+  wl-clipboard
+  rofi
+  waybar
+  waypaper
+  wallust
+  matugen
+)
+
+for pkg in $HYPR; do
+  yay -S --needed --noconfirm "$pkg" ||
+    printf "\n${R}Failed to install $pkg. Continuing without!${C}\n"
+done
+
+printf "\n${G}Linking Hyperland config files${C}\n"
+ln -s ~/dotfiles/config/.config/hypr ~/.config/hypr
