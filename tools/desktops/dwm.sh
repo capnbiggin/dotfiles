9c21fc5898b90e14b10cb3e1b5269881c226b929
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-10-23 21:54:37 -0500

    automatic commit

--- tools/desktops/dwm.sh
+++ tools/desktops/dwm.sh
@@ -25,35 +25,35 @@ XORG=(
   )
 
 for pkg in "${UTILS[@]}"; do
-  yay -S --needed --noconfirm "$pkg" ||
+  sudo pacman -S --needed --noconfirm "$pkg" ||
     printf "\n${R}Failed to install $pkg. Continuing without!${C}\n"
 done
 
 for pkg in "${BDETS[@]}" ; do
-  yay -S --needed --noconfirm "$pkg" ||
+  sudo pacman -S --needed --noconfirm "$pkg" ||
     printf "\n${R}Failed to install $pkg. Continuing without!${C}\n"
 done
 
 
 for pkg in "${XORG[@]}" ; do
-  yay -S --needed --noconfirm "$pkg" ||
+  sudo pacman -S --needed --noconfirm "$pkg" ||
     printf "\n${R}Failed to install $pkg. Continuing without!${C}\n"
 done
-
-mkdir -p ~/.local/share/suckless
-
-git clone git@github.com:capnbiggin/suckless.git ~/.local/share/suckless
-
-cd ~/.local/share/suckless/dwm
-
-sudo make clean install
-
-cd ~/.local/share/suckless/dmenu
-
-sudo make clean install
-
-cd ~/.local/share/suckless/st
-
-sudo make clean install
-
-cd ~
+#
+#mkdir -p ~/.local/share/suckless
+#
+#git clone git@github.com:capnbiggin/suckless.git ~/.local/share/suckless
+#
+#cd ~/.local/share/suckless/dwm
+#
+#sudo make clean install
+#
+#cd ~/.local/share/suckless/dmenu
+#
+#sudo make clean install
+#
+#cd ~/.local/share/suckless/st
+#
+#sudo make clean install
+#
+#cd ~
