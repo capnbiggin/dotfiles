2e34d2fd75471e70655d876168d4993f0e62b85c
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-15 15:46:36 -0600

    automatic commit

--- tools/install.sh
+++ tools/install.sh
@@ -43,6 +43,7 @@ printf "\nInstalling Desktop...\n"
 source $CDOT_INSTALL/desktop/webapps.sh
 source $CDOT_INSTALL/desktop/fonts.sh
 source $CDOT_INSTALL/desktop/browsers.sh
+source $CDOT_INSTALL/desktop/screen-cap.sh
 
 # Dev
 show_logo
