8d6c2b281fcfbe4ca8081f14e77462bfa98ccad3
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-19 21:42:06 -0600

    Add Documents/scripts/projects_menu.sh

---
+++ Documents/scripts/executable_projects_menu.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+# Set your terminal:
+terminal="ghostty"
+dev="$HOME/Documents/dev"
+
+# Pick repo
+configs="$(ls -1d $dev/*/ 2>/dev/null | xargs -n1 basename)"
+[ -n "$configs" ] || exit 0
+chosen="$(printf '%s\n' $configs | rofi -dmenu -p 'Projects:')"
+[ -n "$chosen" ] || exit 0
+dir="$HOME/Documents/dev/$chosen"
+
+# Nuke any existing st (since you only use one terminal)
+pkill -x $terminal 2>/dev/null || true
+sleep 0.1
+
+# Launch a clean terminal: attach if exists, else create
+# exec st -e tmux new-session -As "$chosen" -c "$dir" "nvim ."
+exec $terminal -e tmux new-session -As "$chosen" -c "$dir"
