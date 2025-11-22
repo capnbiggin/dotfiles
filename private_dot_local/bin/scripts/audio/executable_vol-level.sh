3b703ba67f791739f9e3fc3b82b5148c04f2a1f6
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-21 20:52:06 -0600

    Remove .local/bin/scripts/audio/mic-mute.sh Remove .local/bin/scripts/audio/vol-down.sh Remove .local/bin/scripts/audio/vol-level.sh Remove .local/bin/scripts/audio/vol-mute.sh Remove .local/bin/scripts/audio/vol-up.sh Remove .local/bin/scripts/audio/vol.sh Remove .local/bin/scripts/path.sh

--- private_dot_local/bin/scripts/audio/executable_vol-level.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mute_status=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf $3}')
-if [[ $mute_status != "" ]]; then
-    printf "MUTED"
-    exit 0
-fi
-wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%s%\n", $2*100}'
