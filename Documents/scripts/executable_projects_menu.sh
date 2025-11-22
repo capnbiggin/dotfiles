4ea83d981e00be038502d2c050523f23a06b57a4
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-22 17:22:59 -0600

    Update Documents/scripts/projects_menu.sh

--- Documents/scripts/executable_projects_menu.sh
+++ Documents/scripts/executable_projects_menu.sh
@@ -11,7 +11,7 @@ configs="$(find "$dev"/*/ -maxdepth 0 -print0 | xargs -0 -n1 basename)"
 [ -n "$configs" ] || exit 0
 chosen="$(printf '%s\n' "$configs" | rofi -dmenu -p 'Projects:')"
 [ -n "$chosen" ] || exit 0
-dir="$HOME/Documents/dev/$chosen"
+dir="$dev/$chosen"
 
 # Nuke any existing terminal (since you only use one terminal)
 # pkill -x $terminal 2>/dev/null || true
