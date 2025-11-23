7d7004a9f46630b27888bab596a17d050b47f3e3
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-23 10:01:50 -0600

    Add Documents/scripts/install-scripts/config/backlight.sh Add Documents/scripts/install-scripts/config/bluetooth.sh Add Documents/scripts/install-scripts/config/config.sh Add Documents/scripts/install-scripts/config/network.sh Add Documents/scripts/install-scripts/config/power.sh Add Documents/scripts/install-scripts/config/zsh-plugins.sh Add Documents/scripts/install-scripts/config/zsh.sh

---
+++ Documents/scripts/install-scripts/config/executable_power.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+yay -S --noconfirm power-profiles-daemon
+
+if ls /sys/class/power_supply/BAT* &>/dev/null; then
+  # This computer runs on a battery
+  powerprofilesctl set balanced || true
+else
+  # This computer runs on power outlet
+  powerprofilesctl set performance || true
+fi
