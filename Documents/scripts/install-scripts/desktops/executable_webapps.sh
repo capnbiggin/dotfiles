546a1b0988f6194ca3084ad43c9bd05baed1f553
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-23 11:06:29 -0600

    Update Documents/scripts/install-scripts/desktops/webapps.sh

--- Documents/scripts/install-scripts/desktops/executable_webapps.sh
+++ Documents/scripts/install-scripts/desktops/executable_webapps.sh
@@ -10,5 +10,6 @@ source $WEBAPP_INSTALL "Google Messages" https://messages.google.com/web/convers
 source $WEBAPP_INSTALL "YouTube" https://youtube.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
 source $WEBAPP_INSTALL "GitHub" https://github.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
 source $WEBAPP_INSTALL "LastPass" https://lastpass.com/vault/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/lastpass.png
+source $WEBAPP_INSTALL "Discord" https://discord.com/channels/@me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
 
 update-desktop-database "$WEBAPP_DIR"
