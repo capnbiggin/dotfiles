4cb9fff239ee12d76da404a925639be937b96927
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2025-11-23 20:18:31 -0600

    Update Documents/scripts/install-scripts/config/all.sh

--- Documents/scripts/install-scripts/config/executable_all.sh
+++ Documents/scripts/install-scripts/config/executable_all.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 CFG_INSTALL_DIR=$HOME/Documents/scripts/install-scripts/config
+
+source "$CFG_INSTALL_DIR"/config.sh
+source "$CFG_INSTALL_DIR"/zsh-plugins.sh
+source "$CFG_INSTALL_DIR"/zsh.sh
+source "$CFG_INSTALL_DIR"/newtwork.sh
+source "$CFG_INSTALL_DIR"/bluetooth.sh
+source "$CFG_INSTALL_DIR"/backlight.sh
+source "$CFG_INSTALL_DIR"/power.sh
