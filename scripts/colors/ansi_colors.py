0c03935ab52827ac12c82d60b16333665925352f
capnbiggin <3750145+capnbiggin@users.noreply.github.com>
2026-08-17 21:42:26 -0500

    - Mon Aug 17 09:42:26 PM CDT 2026

--- scripts/colors/ansi_colors.py
+++ scripts/colors/ansi_colors.py
@@ -4,11 +4,17 @@
 # Posted by Richard, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-07-19, License - CC BY-SA 4.0
 
+print("\n\033[1;4mForeground Colors\033[21;24m")
 for i in range(30, 37 + 1):
-    print("\033[%dm%d\t\t\033[%dm%d" % (i, i, i + 60, i + 60))
+    print("\033[%dm\\033[%dm\t\t\033[%dm\\033[%dm\033[0m" % (i, i, i + 60, i + 60))
 
+print("\n\033[1;4mBackground Colors\033[21;24m")
+for i in range(40, 47 + 1):
+    print("\033[30;%dm\\033[%dm\t\t\033[%dm\\033[%dm\033[0m" % (i, i, i + 60, i + 60))
 
-print("\\033[39m\\033[49m                 - Reset color")
+print("\n\\033[0m                          - Reset all color")
+print("\\033[39m or \\033[99m             - Reset forground color")
+print("\\033[49m or \\033[109m            - Reset background color\n")
 print("\\033[2K                          - Clear Line")
 print("\\033[<L>;<C>H or \\033[<L>;<C>f   - Put the cursor at line L and column C.")
 print("\\033[<N>A                        - Move the cursor up N lines")
@@ -23,3 +29,4 @@ print("\\033[4m                          - Underline on")
 print("\\033[24m                         - Underline off\n")
 print("\\033[1m                          - Bold on")
 print("\\033[21m                         - Bold off")
+
